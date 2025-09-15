fa78148a5f63d10300b096d3db4106ed0649b0db
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-09-15 12:53:44 +0100

    First React code

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+  <h1> This is a test to see if im good at react </h1>)
