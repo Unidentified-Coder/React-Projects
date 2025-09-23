48f4f73daeff4624882a86c871640a5fc66a198f
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-09-23 21:11:22 +0100

    Added Pic + Main page

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,31 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
+
+function MainPage(){
+  return(
+    <div>
+      <header>
+        <img src="src/assets/Rodas-Grad-Pic.JPG" width="100px" alt="Rodas in Graduation Attire"/>
+      </header>
+      <main>
+        <h1>
+          Who i am
+        </h1>
+        <h2>
+          Name's Rodas I'm a Frontend/Web Developer
+        </h2>
+        <ul>
+          <li>G</li>
+        </ul>
+      </main>
+      <footer>
+        "© 2025 Rodz development. All rights reserved."
+      </footer>
+    </div>
+  )
+}
+
 root.render(
-  <h1> This is a test to see if im good at react </h1>)
+  < MainPage />
+)
