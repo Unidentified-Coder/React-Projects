4aaf111a1b121d15f231fd55d526deb4bea38bbd
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-09-28 17:15:05 +0100

    Footer Postion fix + Alignment of Navbar and Logo

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,52 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
 
-function MainPage(){
-  return(
-    <div>
-      <header>
+function Header(){
+  return (
+      <header className="header">
         <img src="src/assets/Rodas-Grad-Pic.JPG" width="100px" alt="Rodas in Graduation Attire"/>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Home</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact me</li>
+
+        </ul>
+      </nav>
       </header>
+
+  )
+}
+function MainContent(){
+  return(
       <main>
         <h1>
           Who i am
         </h1>
-        <h2>
-          Name's Rodas I'm a Frontend/Web Developer
-        </h2>
-        <ul>
-          <li>G</li>
-        </ul>
+          <h2>
+            Name's Rodas I'm a Frontend/Web Developer
+          </h2>
+          <ul>
+            <li>Graduated with a 2.1 in Software Engineering</li>
+          </ul>
       </main>
-      <footer>
-        "© 2025 Rodz development. All rights reserved."
+
+  )
+}
+function Footer(){
+  return(
+     <footer className="Legals">
+        © 2025 Rodz development. All rights reserved."dont touch my shxt nxggaxs"
       </footer>
-    </div>
+  )
+}
+function MainPage(){
+  return(
+    <>
+    <Header />
+    <MainContent />
+    <Footer />
+    </>
   )
 }
 
