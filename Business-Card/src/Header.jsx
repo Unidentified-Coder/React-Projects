e2a0cce70ca6e0309a1068ef4e691d9b663d8b88
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-10-03 17:44:03 +0100

    test

---
+++ Business-Card/src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+  return (
+      <header className="header">
+        <img src="src/assets/Rodas-Grad-Pic.JPG" width="100px" alt="Rodas in Graduation Attire"/>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Home</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact me</li>
+
+        </ul>
+      </nav>
+      </header>
+
+  )
+}
--- Business-Card/src/main.jsx
+++ Business-Card/src/main.jsx
@@ -1,24 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-
+import Header from "./Header"
 const root = createRoot(document.getElementById("root"))
 
-function Header(){
-  return (
-      <header className="header">
-        <img src="src/assets/Rodas-Grad-Pic.JPG" width="100px" alt="Rodas in Graduation Attire"/>
-      <nav>
-        <ul className="nav-list">
-          <li className="nav-list-item">Home</li>
-          <li className="nav-list-item">About</li>
-          <li className="nav-list-item">Contact me</li>
-
-        </ul>
-      </nav>
-      </header>
-
-  )
-}
 function MainContent(){
   return(
       <main>
