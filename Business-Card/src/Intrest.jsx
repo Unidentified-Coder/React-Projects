7a198a164bcaa9f6173000ebc32ebdae977ba74a
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-10-11 17:03:35 +0100

    CSS Work Resized Image + text alignment

--- Business-Card/src/Intrest.jsx
+++ Business-Card/src/Intrest.jsx
@@ -8,6 +8,7 @@ export default function Intrest(){
                 <li>My favourite team is Chelsea</li>
                 <li>I love to do the complicated stuff</li>
                 <li>Gaming fnatic</li>
+                <li>Always on the internet trying to adapt to new challenges along the way</li>
             </ul>
         </>
     )
