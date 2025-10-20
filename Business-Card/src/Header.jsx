f20010e1d2edd745f2403a20a62ecf1fb36a09d4
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-10-20 10:50:58 +0100

    Base Layout  ✅

--- Business-Card/src/Header.jsx
+++ Business-Card/src/Header.jsx
@@ -1,64 +1,71 @@
 import './Components/index.css'
 export default function Header(){
   return (
-    <>
-      <header className="header">
-        <img className= 'main-img' src="src/assets/Rodas-Grad-Pic.JPG" alt="Rodas in Graduation Attire"/>
-      </header>
+    <div className='Main-Container'>
+        <div className='Business-Card'>
 
+        <header className="header">
+            <img className= 'main-img' src="src/assets/Rodas-Grad-Pic.JPG" alt="Rodas in Graduation Attire"/>
+        </header>
 
-        <h1 className='Name-ttl'>Rodas Samson Gebrtensie</h1>
-        <h3 className='Sub-ttl'>Frontend Developer</h3>
-        
-        <div className='Buttons'>
+        <div className='Secondary-Container'>
 
-            <a 
-                className="email-btn"
-                
-                href = "mailto:rsamson139@gmail.com"
-                target="_blank"
-                >
-                
-                
-                <img className="email-img-icon" src="src/assets/Icon.png" alt="mail logo" />
-            <span className='email-header'>Email</span>
-            </a>
+            <h1 className='Name-ttl'>Rodas Samson Gebrtensie</h1>
+            <h3 className='Sub-ttl'>Frontend Developer</h3>
             
-            <a 
-                className="linkedin-btn"
+            <div className='Buttons'>
+
+                <a 
+                    className="email-btn"
+                    
+                    href = "mailto:rsamson139@gmail.com"
+                    target="_blank"
+                    >
+                    
+                    
+                    <img className="email-img-icon" src="src/assets/Icon.png" alt="mail logo" />
+                <span className='email-header'>Email</span>
+                </a>
                 
-                href="https://www.linkedin.com/in/rodas-samson-gebrtensea-8a53922a9"
-                target="_blank"
-                >
-                <img className="li-img-icon" src="src/assets/linkedin.png" alt="blue l" />
-                <span className='linkedin-header'>Linkedin</span>
-            </a>
-        </div>
-        
-        <div className='About-info'>
-            <h1 className='title-txt'>
-                About
+                <a 
+                    className="linkedin-btn"
+                    
+                    href="https://www.linkedin.com/in/rodas-samson-gebrtensea-8a53922a9"
+                    target="_blank"
+                    >
+                    <img className="li-img-icon" src="src/assets/linkedin.png" alt="blue l" />
+                    <span className='linkedin-header'>Linkedin</span>
+                </a>
+            </div>
+            
+            <div className='About-info'>
+                <h1 className='title-txt'>
+                    About
 
+                </h1>
+                <span className='about-txt'>
+                    I'm a Frontend / Web Developer with intrests in UI/UX. 
+                    I also like making complicated things very simple. 
+                    I'm always open to suggestion in improving my coding techniques 
+                </span>
+            </div>
+
+            <h1 className='Intrest-ttl'>
+                Intrests
+            
             </h1>
-            <span className='about-txt'>
-                I'm a Frontend / Web Developer with intrests in UI/UX. 
-                I also like making complicated things very simple. 
-                I'm always open to suggestion in improving my coding techniques 
-            </span>
+
+                <ul>
+                    <li>My favourite team is Chelsea</li>
+                    <li>I love to do the complicated stuff</li>
+                    <li>Gaming fnatic</li>
+                    <li>Always on the internet trying to adapt to new challenges along the way</li>
+                </ul>
+            </div>
         </div>
+    </div>    
 
-        <h1 className='Intrest-ttl'>
-            Intrests
-        
-        </h1>
 
-            <ul>
-                <li>My favourite team is Chelsea</li>
-                <li>I love to do the complicated stuff</li>
-                <li>Gaming fnatic</li>
-                <li>Always on the internet trying to adapt to new challenges along the way</li>
-            </ul>
-    </>
 
   )
 }
