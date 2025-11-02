c1d2864ec8062ffb889773138883bd5a230213f2
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-11-02 10:49:23 +0000

    ⬆️

---
+++ Business-Card/src/Intrest.jsx
@@ -0,0 +1,14 @@
+import './Components/Intrest.css'
+export default function Intrest(){
+    return(
+        <>
+        <h1 className='Intrest-ttl'>Intrests</h1>
+
+            <ul>
+                <li>My favourite team is Chelsea</li>
+                <li>I love to do the complicated stuff</li>
+                <li>Gaming fnatic</li>
+            </ul>
+        </>
+    )
+}
