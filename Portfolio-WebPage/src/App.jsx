cdff2bcf5bd991bb2900279d0b4ed675e0d15d1d
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2025-11-15 18:14:55 +0000

    Basic Template ✅

--- Portfolio-WebPage/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App(){
-  return(
-    <h1 className="text-3xl font-bold underline text-blue-900 pt-4 pr-5">
-        Hello World!
-    </h1>
-  )
-}
---
+++ Portfolio-WebPage/src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+  return(
+    <header className="flex justify-around">
+      <h1>
+        Logo
+      </h1>
+      <h1 className= "flex justify-center-safe">
+          Rodz Development
+      </h1>
+      <h1>
+        Navbar
+      </h1>
+
+    </header>
+  )
+}
